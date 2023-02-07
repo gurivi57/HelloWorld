6b20ee4bc3e8fe5372b53a78538d528c062dff07
gurivi57 <gurivireddy.4ld@gmail.com>
2023-02-07 15:49:22 +1100

    First Git Commit

---
+++ src/GitCommit.java
@@ -0,0 +1,7 @@
+public class GitCommit {
+
+    public static void main (String [] args){
+        System.out.println("Hello Git, this is my first remote commit");
+        System.out.println("Hello Git, this is my second remote commit");
+    }
+}

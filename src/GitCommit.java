d9c433de269329fab5a04659f3587d076fc63b14
gurivi57 <gurivireddy.4ld@gmail.com>
2023-02-07 15:56:39 +1100

    Modified Git Commit

--- src/GitCommit.java
+++ src/GitCommit.java
@@ -2,6 +2,6 @@ public class GitCommit {
 
     public static void main (String [] args){
         System.out.println("Hello Git, this is my first remote commit");
-        System.out.println("Hello Git, this is my second remote commit");
+        //Removed extra print message
     }
 }

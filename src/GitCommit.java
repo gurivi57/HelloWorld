ef49a7a26500ae7e39c51af8abbafaf84d7ff33d
gurivi57 <gurivireddy.4ld@gmail.com>
2023-02-13 11:52:30 +1100

    Updated Git Commit

--- src/GitCommit.java
+++ src/GitCommit.java
@@ -6,5 +6,13 @@ public class GitCommit {
         System.out.println("Hello Git, this is my first remote commit");
         //Removed extra print message
         System.out.println("Testing git commit");
+        GitCommit.add();
+    }
+
+    public static void add(){
+        int a=1;
+        int b=3;
+        int total=a+b;
+        System.out.println("total is " +total);
     }
 }
